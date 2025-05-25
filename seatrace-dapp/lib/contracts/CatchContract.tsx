45dee664a79c4aaa651dec7e6f01ab4f6cbae6d7
Aaron H <115131492+Azar3211@users.noreply.github.com>
2025-05-25 18:18:17 +1000

    Delete seatrace-dapp directory

--- seatrace-dapp/lib/contracts/CatchContract.tsx
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import CatchContractJSON from "./CatchContract.json";
-
-const CONTRACT_ADDRESS = "0x1c036CA613f1DADb37C08d0454F20E15F2f52eE1"; 
-
-export function getCatchContract(providerOrSigner: ethers.Provider | ethers.Signer) {
-    return new ethers.Contract(CONTRACT_ADDRESS, CatchContractJSON.abi, providerOrSigner);
-}
